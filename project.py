19ca67d506127c7a3ec36d7fcad7162466b083df
Violetta Volodina <dsw48795@student.dsw.edu.pl>
2023-06-07 02:47:59 +0200

    wczytywanie do obiektu z pliku .yml i weryfikacja

--- project.py
+++
@@ -1,9 +0,0 @@
-#Task0
-#pyinstaller.exe --onefile project.py
-#pyinstaller.exe --onefile --noconsole project.py
-#Task1
-#pip install argparse
-#Task2,3
-#pip install json
-#Task4
-#pip install pyyaml
