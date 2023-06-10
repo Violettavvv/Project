f5a2e11533187282f3f2d1b13ad6f2480e7f97ec
Violetta Volodina <dsw48795@student.dsw.edu.pl>
2023-06-10 02:16:50 +0200

    wczytywanie do obiektu z pliku .json i weryfikacja poprawności składni pliku

--- Task2code.py
+++ Task2code.py
@@ -11,7 +11,7 @@ def load_json_file(file_path):
         print("Błąd dekodowania pliku JSON:")
         print(e)
 
-file_path = 'sciezka/do/pliku.json'
+file_path = 'Sciezka/do/pliku.json'
 json_data = load_json_file(file_path)
 
 if json_data is not None:
