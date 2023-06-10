cfcce64857d7a0b1565c60f61611264c1abbf00a
Violetta Volodina <dsw48795@student.dsw.edu.pl>
2023-06-10 02:25:08 +0200

    zapis danych z obiektu do pliku w formacie i zgodnie ze składnią .yml

--- Task5code.py
+++ Task5code.py
@@ -3,7 +3,7 @@ import yaml
 produkty = {
     'nazwa': 'Produkt 1',
     'cena': 7.99,
-    'kategoria': ['slodycze', 'pieczywo']
+    'kategoria': ['słodycze', 'pieczywo']
 }
 
 with open('plik.yml', 'w') as file:
